8a3c3a8f1425e4e286021b4418a7442a3c3b2c65
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 13:38:15 +0000

    add functionality to link to github

--- src/Main Screen/Components/Header.tsx
+++ src/Main Screen/Components/Header.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { Github } from "grommet-icons";
 
 const HeaderWrap = styled.header``;
+
+const LinksWrap = styled.nav``;
 
 const Header = () => {
   return (
     <HeaderWrap>
       <h1>Richard James</h1>
       <h2>Front End Developer</h2>
+      <LinksWrap>
+        <a
+          aria-label="Github link"
+          href="https://github.com/richpjames/"
+          color="#000000"
+        >
+          <Github />
+        </a>
+      </LinksWrap>
     </HeaderWrap>
   );
 };
