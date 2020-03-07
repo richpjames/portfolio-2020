6d90c18e8fda7ce25501f5b024f74fc6a8a9bd3c
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 17:33:07 +0000

    add test to check child is rendered

--- src/Main Screen/Work/WorkSegment.test.tsx
+++ src/Main Screen/Work/WorkSegment.test.tsx
@@ -14,4 +14,13 @@ describe("WorkSegment", () => {
 
     expect(getByText(title)).toBeInTheDocument();
   });
+  it("displays children correctly", () => {
+    const title = "This is my title";
+    const child = <p title="description">this is the child</p>;
+    const { getByTitle } = render(
+      <WorkSegment title={title}>{child}</WorkSegment>
+    );
+
+    expect(getByTitle("description"));
+  });
 });
