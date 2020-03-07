15b40ad79c7b80c7350355b6686b4c57161a8ac8
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 12:06:25 +0000

    add components

--- src/Main Screen/MainScreen.tsx
+++ src/Main Screen/MainScreen.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import Bio from "./Components/Bio";
+import Header from "./Components/Header";
+import Experience from "./Components/Experience";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import Footer from "./Components/Footer";
+
 const MainScreen = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Bio />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Footer />
+    </>
+  );
 };
 
 export default MainScreen;
