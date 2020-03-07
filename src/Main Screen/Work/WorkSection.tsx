f7cd3673c069e8742626f1aaf4bce797aee62163
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 17:08:34 +0000

    add content

--- src/Main Screen/Work/WorkSection.tsx
+++ src/Main Screen/Work/WorkSection.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import Experience from "./Experience";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import colours from "../../Styles/Colours";
+
+const WorkWrap = styled.section`
+  display: flex;
+`;
+
+const WorkSegment = styled.div`
+  border-left: 1px solid ${colours.highlight};
+  border-right: 1px solid ${colours.highlight};
+
+  overflow-y: scroll;
+`;
 
 const WorkSection = () => {
-  return <div></div>;
+  return (
+    <WorkWrap>
+      <WorkSegment>
+        <Experience />
+      </WorkSegment>
+      <WorkSegment>
+        <Projects />
+      </WorkSegment>
+      <WorkSegment>
+        <Skills />
+      </WorkSegment>
+    </WorkWrap>
+  );
 };
 
 export default WorkSection;
