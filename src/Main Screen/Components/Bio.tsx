dadd10e562974aa652d4b9154f6adc5cd9b0d575
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 16:13:48 +0000

    add content and style

--- src/Main Screen/Components/Bio.tsx
+++ src/Main Screen/Components/Bio.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+const BioWrap = styled.section`
+  height: 200px;
+  width: 50%;
+  margin-top: 5%;
+  text-align: center;
+`;
 
 const Bio = () => {
-  return <div></div>;
+  return (
+    <BioWrap title="bio">
+      <p>
+        I’m a fast-learner who is passionate about designing and developing
+        beautiful user interfaces that stimulate memorable interactions, tell
+        stories and make products come to life. I love leveraging tech to create
+        elegant solutions to difficult problems.
+      </p>
+    </BioWrap>
+  );
 };
 
 export default Bio;
