64e5eab0d9837a8d30d76c83e3aad3be3348ca26
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-07 17:08:18 +0000

    add demo content

--- src/Main Screen/Work/Projects.tsx
+++ src/Main Screen/Work/Projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Projects = () => {
-  return <div></div>;
+  return <div>this is my projects</div>;
 };
 
 export default Projects;
--- src/Main Screen/Work/Skills.tsx
+++ src/Main Screen/Work/Skills.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Skills = () => {
-  return <div></div>;
+  return <div>this is my skills</div>;
 };
 
 export default Skills;
