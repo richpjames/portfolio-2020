7a311e0b3e80d74c97ff9f285aa2601447bc5129
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-08 11:44:13 +0000

    add skills list and icons

--- src/Main Screen/Work/Skills.tsx
+++ src/Main Screen/Work/Skills.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Volume, Troubleshoot, Test, Archive, Brush } from "grommet-icons";
+import colours from "../../Styles/Colours";
 
 const Skills = () => {
-  return <div>this is my skills</div>;
+  return (
+    <ul>
+      <li>
+        <Volume color={colours.text} />
+        JavaScript, Typescript, Node, Dart
+      </li>
+      <li>
+        <Troubleshoot color={colours.text} />
+        React, Flutter, HTML, CSS, Styled Components
+      </li>
+      <li>
+        <Test color={colours.text} /> Jest, Cypress, Chai, Mocha
+      </li>
+      <li>
+        <Archive color={colours.text} />
+        Git and Github
+      </li>
+      <li>
+        <Brush color={colours.text} /> Adobe Photoshop and XD
+      </li>
+    </ul>
+  );
 };
 
 export default Skills;
