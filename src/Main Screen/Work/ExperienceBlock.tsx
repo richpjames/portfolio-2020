2aacf38dddb8274c445b3a90692f5cd7672221c2
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-08 19:02:11 +0000

    create experience block

---
+++ src/Main Screen/Work/ExperienceBlock.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import colours from "../../Styles/Colours";
+
+const ExperienceWrap = styled.section`
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const HeadingWrap = styled.div`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  :nth-of-type() {
+    margin-top: 200px;
+  }
+`;
+
+const CompanyTitle = styled.h3`
+  margin-bottom: 5.5px;
+`;
+const Date = styled.h4`
+  margin-bottom: 8.5px;
+`;
+const TextBlock = styled.p`
+  font-size: 18px;
+`;
+
+const Seperator = styled.div`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  border-width: 0 0 2.5px;
+  border-style: solid;
+  border-color: ${colours.highlight};
+`;
+
+interface IProps {
+  title: string;
+  date?: string;
+  description: JSX.Element;
+}
+
+const ExperienceBlock: React.FC<IProps> = (props: IProps) => {
+  const { title, description, date } = props;
+  return (
+    <ExperienceWrap>
+      <HeadingWrap>
+        <CompanyTitle>{title}</CompanyTitle>
+        <Date>{date}</Date>
+      </HeadingWrap>
+      <TextBlock>{description}</TextBlock>
+      <Seperator />
+    </ExperienceWrap>
+  );
+};
+
+export default ExperienceBlock;
