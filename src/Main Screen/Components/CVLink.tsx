513d7a41b51ec3c494ca6ef8aa2e9fc892f91e89
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2020-03-10 20:45:50 +0000

    add CV Icon

--- src/Main Screen/Components/CVLink.tsx
+++ src/Main Screen/Components/CVLink.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
+import { DocumentText } from "grommet-icons";
+import colours from "../../Styles/Colours";
 
 const CVWrap = styled.div`
   font-size: 21px;
   margin-top: 8vh;
 `;
 
+const TextWrap = styled.a`
+  margin-right: 10px;
+  margin-bottom: 2px;
+`;
 const CVLink = () => {
   return (
     <CVWrap aria-label="link to cv">
-      <a href="https://richjames.co.uk/cv.pdf">View my full CV</a>
+      <TextWrap href="https://richjames.co.uk/cv.pdf">View my full CV</TextWrap>
+      <DocumentText color={colours.text} />
     </CVWrap>
   );
 };
